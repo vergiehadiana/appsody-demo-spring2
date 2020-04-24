9084f0eccfbe7190f3b54c6d79b3155eb31a56ed
vergie <vergie@ubuntu-vergie.localdomain>
2020-04-24 03:06:49 -0700

    Push Fix

--- src/main/java/application/ExampleEndpoint.java
+++ src/main/java/application/ExampleEndpoint.java
@@ -8,6 +8,6 @@ public class ExampleEndpoint {
 
     @RequestMapping("/starter/hello")
     public String example() {
-        return "Hello World Vergie";
+        return "Hello World Vergie SWG";
     }
 }
